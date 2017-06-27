51045085e564d851d6fb809cd887248387e660f1
Fernão Lopes <fernao@riseup.net>
2017-06-27 14:31:53 -0300

    Alterações para docker do valecultura-dashboard

--- src/actions/apache.sh
+++ src/actions/apache.sh
@@ -2,9 +2,6 @@
 echo "[******] Copying and enable virtualhost 'site.conf'";
 cp /tmp/src/actions/apache2/sites-available/site.conf /etc/apache2/sites-available/site.conf
 
-echo "[******] Copying and enable virtualhost 'status.conf'";
-cp /tmp/src/actions/apache2/sites-available/status.conf /etc/apache2/sites-available/status.conf
-
 echo "[******] Copying configurations for security of apache.";
 cp /tmp/src/actions/apache2/conf-available/security.conf /etc/apache2/conf-available/security.conf
 
@@ -12,20 +9,12 @@ echo "[******] Copying configurations for security of apache.";
 cp /tmp/src/actions/apache2/ports.conf /etc/apache2/ports.conf
 
 a2ensite site.conf
-a2ensite status.conf
 
 echo "[******] Disable default virtualhost '000-default.conf'";
 a2dissite 000-default.conf
-
-echo "[******] Enable Apache Mod Rewrite";
-a2enmod rewrite
-
-echo "[******] Enable Apache Mod Headers";
-a2enmod headers
 
 echo "[******] Restarting Apache 2 Service";
 service apache2 reload
 
 echo "[******] Starts Apache using Foreground Mode";
 apache2ctl -D FOREGROUND
-#exec apache2 -DFOREGROUND
